d88c59e429c4cc2ba0b999a2e66382648a8fd56f
vagrant <vagrant@localhost.localdomain>
2016-03-16 18:50:37 +0000

    added opt-out option for timestamp to single and roll-over files

--- src/lib.rs
+++ src/lib.rs
@@ -56,10 +56,10 @@ use std::cell::RefCell;
 use std::cmp::max;
 use std::env;
 use std::fmt;
-use std::fs::{create_dir_all,File};
+use std::fs::{create_dir_all, File};
 use std::io;
-use std::io::{stderr, LineWriter, Write};
-use std::ops::{Add,DerefMut};
+use std::io::{LineWriter, Write};
+use std::ops::{Add, DerefMut};
 use std::path::Path;
 use std::sync::{Arc, Mutex};
 
@@ -87,41 +87,49 @@ struct LwHandle {
     rotate_idx: usize,
 }
 impl LwHandle {
-    fn new (config: &LogConfig) -> Result<LwHandle,FlexiLoggerError> {
+    fn new(config: &LogConfig) -> Result<LwHandle, FlexiLoggerError> {
         if !config.log_to_file {
             // we don't need a line-writer, so we return an empty handle
-            return Ok(LwHandle{
-                olw: None, o_filename_base: None, use_rotating: false, written_bytes: 0, rotate_idx: 0
+            return Ok(LwHandle {
+                olw: None,
+                o_filename_base: None,
+                use_rotating: false,
+                written_bytes: 0,
+                rotate_idx: 0,
             });
         }
 
         // make sure the folder exists or can be created
         let s_directory: String = match config.directory {
             Some(ref dir) => dir.clone(),
-            None => ".".to_string()
+            None => ".".to_string(),
         };
         let directory = Path::new(&s_directory);
 
         if let Err(e) = create_dir_all(&directory) {
-            return Err(FlexiLoggerError::new(
-                format!("Log cannot be written: output directory \"{}\" does not \
-                exist and could not be created due to {}", &directory.display(),e)));
+            return Err(FlexiLoggerError::new(format!("Log cannot be written: output directory \
+                                                      \"{}\" does not exist and could not be \
+                                                      created due to {}",
+                                                     &directory.display(),
+                                                     e)));
         };
 
         let o_filename_base = match std::fs::metadata(&directory) {
             Ok(metadata) => {
                 if metadata.is_dir() {
-                    Some(get_filename_base(&s_directory.clone(), & config.discriminant))
+                    Some(get_filename_base(&s_directory.clone(), &config.discriminant))
                 } else {
-                    return Err(FlexiLoggerError::new(
-                        format!("Log cannot be written: output directory \"{}\" is not \
-                        a directory", &directory.display())));
+                    return Err(FlexiLoggerError::new(format!("Log cannot be written: output \
+                                                              directory \"{}\" is not a \
+                                                              directory",
+                                                             &directory.display())));
                 }
-            },
+            }
             Err(e) => {
-                return Err(FlexiLoggerError::new(
-                    format!("Log cannot be written: error accessing output directory \"{}\": {}",
-                    &directory.display(), e)));
+                return Err(FlexiLoggerError::new(format!("Log cannot be written: error \
+                                                          accessing output directory \"{}\": {}",
+                                                         &directory.display(),
+                                                         e)));
             }
         };
 
@@ -130,26 +138,33 @@ impl LwHandle {
             Some(ref s_filename_base) => {
                 match config.rotate_over_size {
                     None => (false, 0),
-                    Some(_) => (true, get_next_rotate_idx(&s_filename_base, & config.suffix))
+                    Some(_) => (true, get_next_rotate_idx(&s_filename_base, &config.suffix)),
                 }
             }
         };
 
-        let mut lwh = LwHandle{
+        let mut lwh = LwHandle {
             olw: None,
             o_filename_base: o_filename_base,
             use_rotating: use_rotating,
             written_bytes: 0,
             rotate_idx: rotate_idx,
         };
-        lwh.mount_linewriter(&config.suffix, config.print_message);
+        lwh.mount_linewriter(&config.suffix, &config.timestamp, config.print_message);
         Ok(lwh)
     }
 
-    fn mount_linewriter(&mut self, suffix: &Option<String>, print_message: bool) {
+    fn mount_linewriter(&mut self,
+                        suffix: &Option<String>,
+                        timestamp: &Option<bool>,
+                        print_message: bool) {
         if let None = self.olw {
             if let Some(ref s_filename_base) = self.o_filename_base {
-                let filename = get_filename(s_filename_base, self.use_rotating, self.rotate_idx, suffix);
+                let filename = get_filename(s_filename_base,
+                                            self.use_rotating,
+                                            self.rotate_idx,
+                                            suffix,
+                                            timestamp);
                 let path = Path::new(&filename);
                 if print_message {
                     println!("Log is written to {}", path.display());
@@ -173,12 +188,15 @@ fn get_filename_base(s_directory: &String, discriminant: &Option<String>) -> Str
 fn get_filename(s_filename_base: &String,
                 do_rotating: bool,
                 rotate_idx: usize,
-                o_suffix: &Option<String>) -> String {
+                o_suffix: &Option<String>,
+                timestamp: &Option<bool>)
+                -> String {
     let mut filename = String::with_capacity(180).add(&s_filename_base);
+    if timestamp.is_some() && timestamp.unwrap() {
+        filename = filename.add(&time::strftime("_%Y-%m-%d_%H-%M-%S", &time::now()).unwrap())
+    }
     if do_rotating {
         filename = filename.add(&format!("_r{:0>5}", rotate_idx));
-    } else {
-        filename = filename.add(&time::strftime("_%Y-%m-%d_%H-%M-%S",&time::now()).unwrap());
     }
     if let &Some(ref suffix) = o_suffix {
         filename = filename.add(".").add(suffix);
@@ -196,14 +214,16 @@ fn get_filename_pattern(s_filename_base: &String, o_suffix: &Option<String>) -> 
 }
 
 // FIXME error handling
-fn get_next_rotate_idx(s_filename_base: & String, o_suffix: & Option<String>) -> usize {
+fn get_next_rotate_idx(s_filename_base: &String, o_suffix: &Option<String>) -> usize {
     let fn_pattern = get_filename_pattern(s_filename_base, o_suffix);
     let paths = glob(&fn_pattern);
     let mut rotate_idx = 0;
     match paths {
         Err(e) => {
-            panic!("Is this ({}) really a directory? Listing failed with {}", fn_pattern, e); // FIXME
-        },
+            panic!("Is this ({}) really a directory? Listing failed with {}",
+                   fn_pattern,
+                   e); // FIXME
+        }
         Ok(it) => {
             for globresult in it {
                 match globresult {
@@ -218,26 +238,36 @@ fn get_next_rotate_idx(s_filename_base: & String, o_suffix: & Option<String>) ->
             }
         }
     }
-    rotate_idx+1
+    rotate_idx + 1
 }
 
 
-pub struct FlexiLogger{
+pub struct FlexiLogger {
     directives: Vec<LogDirective>,
     o_filter: Option<Regex>,
     amo_line_writer: Arc<Mutex<RefCell<LwHandle>>>,
-    config: LogConfig
+    config: LogConfig,
 }
 impl FlexiLogger {
-    pub fn new(loglevelspec: Option<String>, config: LogConfig)
-            -> Result<FlexiLogger, FlexiLoggerError>  {
+    pub fn new(loglevelspec: Option<String>,
+               config: LogConfig)
+               -> Result<FlexiLogger, FlexiLoggerError> {
 
         let (mut directives, filter) = match loglevelspec {
-            Some(ref llspec) => {let spec: &str = llspec; parse_logging_spec(&spec)},
+            Some(ref llspec) => {
+                let spec: &str = llspec;
+                parse_logging_spec(&spec)
+            }
             None => {
                 match env::var("RUST_LOG") {
                     Ok(spec) => parse_logging_spec(&spec),
-                    Err(..) => (vec![LogDirective { name: None, level: LogLevelFilter::Error }], None),
+                    Err(..) => {
+                        (vec![LogDirective {
+                                  name: None,
+                                  level: LogLevelFilter::Error,
+                              }],
+                         None)
+                    }
                 }
             }
         };
@@ -252,12 +282,15 @@ impl FlexiLogger {
 
         let lwh = LwHandle::new(&config);
         match lwh {
-            Ok(lwh) =>  Ok(FlexiLogger {
-                            directives: directives,
-                            o_filter: filter,
-                            amo_line_writer: Arc::new(Mutex::new(RefCell::new(lwh))),
-                            config: config }),
-            Err(e) => Err(e)
+            Ok(lwh) => {
+                Ok(FlexiLogger {
+                    directives: directives,
+                    o_filter: filter,
+                    amo_line_writer: Arc::new(Mutex::new(RefCell::new(lwh))),
+                    config: config,
+                })
+            }
+            Err(e) => Err(e),
         }
     }
 
@@ -265,10 +298,8 @@ impl FlexiLogger {
         // Search for the longest match, the vector is assumed to be pre-sorted.
         for directive in self.directives.iter().rev() {
             match directive.name {
-                Some(ref name) if !target.starts_with(&**name) => {},
-                Some(..) | None => {
-                    return level <= directive.level
-                }
+                Some(ref name) if !target.starts_with(&**name) => {}
+                Some(..) | None => return level <= directive.level,
             }
         }
         false
@@ -293,9 +324,9 @@ impl Log for FlexiLogger {
 
         let mut msg = (self.config.format)(record);
         if self.config.log_to_file {
-            if self.config.duplicate_error && record.level() == LogLevel::Error
-            || self.config.duplicate_info  && record.level() == LogLevel::Info {
-                println!("{}",&record.args());
+            if self.config.duplicate_error && record.level() == LogLevel::Error ||
+               self.config.duplicate_info && record.level() == LogLevel::Info {
+                println!("{}", &record.args());
             }
             msg.push('\n');
             let msgb = msg.as_bytes();
@@ -310,16 +341,20 @@ impl Log for FlexiLogger {
                 lwh.olw = None;  // Hope that closes the previous lw
                 lwh.written_bytes = 0;
                 lwh.rotate_idx += 1;
-                lwh.mount_linewriter(&self.config.suffix,  self.config.print_message);
+                lwh.mount_linewriter(&self.config.suffix,
+                                     &self.config.timestamp,
+                                     self.config.print_message);
             }
             if let Some(ref mut lw) = lwh.olw {
-                &lw.write(msgb).unwrap_or_else( |e|{panic!("File logger: write failed with {}",e);} );//FIXME
+                &lw.write(msgb).unwrap_or_else(|e| {
+                    panic!("File logger: write failed with {}", e);
+                });//FIXME
                 if lwh.use_rotating {
                     lwh.written_bytes += msgb.len();
                 }
             };
         } else {
-            let _ = writeln!(&mut io::stderr(), "{}", msg );
+            let _ = writeln!(&mut io::stderr(), "{}", msg);
         }
     }
 }
@@ -328,14 +363,14 @@ impl Log for FlexiLogger {
 /// Describes errors in the initialization of flexi_logger.
 #[derive(Debug)]
 pub struct FlexiLoggerError {
-    message: String
+    message: String,
 }
 impl FlexiLoggerError {
     pub fn new(s: String) -> FlexiLoggerError {
-        FlexiLoggerError {message: s}
+        FlexiLoggerError { message: s }
     }
 }
-impl fmt::Display for  FlexiLoggerError {
+impl fmt::Display for FlexiLoggerError {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "{}", self.message)
     }
@@ -346,7 +381,6 @@ pub struct LogConfig {
     /// Allows providing a custom logline format; by default ```flexi_logger::default_format``` is used.
     /// You can either choose between two predefined variants, ```default_format``` and ```detailed_format```,
     /// or you create and use your own format function with the signature ```fn(&LogRecord) -> String```.
-
     pub format: fn(&LogRecord) -> String,
 
     /// * If `false` (default), the log is written to stderr.
@@ -371,6 +405,9 @@ pub struct LogConfig {
 
     /// Allows specifying the filesystem suffix of the log files (without the dot).  Default is ```log```.
     pub suffix: Option<String>,
+
+    /// Allows specifying whether or not the filename should include a timestamp. Default is '''true'''.
+    pub timestamp: Option<bool>,
 
     /// Allows specifying a maximum size for log files in bytes; when
     /// the specified file size is reached or exceeded, the file will be closed and a new one will be opened.
@@ -401,9 +438,10 @@ impl LogConfig {
             duplicate_info: false,
             format: default_format,
             directory: None,
+            suffix: Some("log".to_string()),
+            timestamp: Some(true),
             rotate_over_size: None,
             discriminant: None,
-            suffix: Some("log".to_string()),
         }
     }
 }
@@ -411,7 +449,10 @@ impl LogConfig {
 /// A logline-formatter that produces lines like <br>
 /// ```INFO [my_prog::some_submodel] Task successfully read from conf.json```
 pub fn default_format(record: &LogRecord) -> String {
-    format!( "{} [{}] {}", record.level(), record.location().module_path(), record.args() )
+    format!("{} [{}] {}",
+            record.level(),
+            record.location().module_path(),
+            record.args())
 }
 
 
@@ -425,15 +466,18 @@ pub fn detailed_format(record: &LogRecord) -> String {
     // ugly code to format milli and micro seconds
     let tmp = 1000000000 + timespec.nsec;
     let mut s = tmp.to_string();
-    s.remove(9);s.remove(8);s.remove(7);s.remove(0);
+    s.remove(9);
+    s.remove(8);
+    s.remove(7);
+    s.remove(0);
     time = time.add(&s);
-    format!( "[{}] {} [{}] {}:{}: {}",
-                &time,
-                record.level(),
-                record.location().module_path(),
-                record.location().file(),
-                record.location().line(),
-                &record.args())
+    format!("[{}] {} [{}] {}:{}: {}",
+            &time,
+            record.level(),
+            record.location().module_path(),
+            record.location().file(),
+            record.location().line(),
+            &record.args())
 }
 
 struct LogDirective {
@@ -501,14 +545,19 @@ struct LogDirective {
 /// Init returns a FlexiLoggerError, if it is supposed to write to an output file
 /// but the file cannot be opened, e.g. because of operating system issues.
 ///
-pub fn init(config: LogConfig, loglevelspec: Option<String>) -> Result<(),FlexiLoggerError> {
-    let result = FlexiLogger::new(loglevelspec,config);
+pub fn init(config: LogConfig, loglevelspec: Option<String>) -> Result<(), FlexiLoggerError> {
+    let result = FlexiLogger::new(loglevelspec, config);
     match result {
         Ok(fl) => {
             let max = fl.directives.iter().map(|d| d.level).max().unwrap_or(LogLevelFilter::Off);
-            log::set_logger( |max_level| {max_level.set(max);Box::new(fl)} )
-                 .map_err(|e|{FlexiLoggerError::new(format!("Logger initialization failed due to {}", e))})
-        },
+            log::set_logger(|max_level| {
+                max_level.set(max);
+                Box::new(fl)
+            })
+                .map_err(|e| {
+                    FlexiLoggerError::new(format!("Logger initialization failed due to {}", e))
+                })
+        }
         Err(e) => Err(e),
     }
 }
@@ -522,7 +571,8 @@ fn parse_logging_spec(spec: &str) -> (Vec<LogDirective>, Option<Regex>) {
     let mods = parts.next();
     let filter = parts.next();
     if parts.next().is_some() {
-        print_err!("warning: invalid logging spec '{}', ignoring it (too many '/'s)", spec);
+        print_err!("warning: invalid logging spec '{}', ignoring it (too many '/'s)",
+                   spec);
         return (dirs, None);
     }
     mods.map(|m| { for s in m.split(',') {
@@ -574,8 +624,8 @@ fn parse_logging_spec(spec: &str) -> (Vec<LogDirective>, Option<Regex>) {
 
 #[cfg(test)]
 mod tests {
-    use log::{LogLevel,LogLevelFilter};
-    use super::{FlexiLogger, LogConfig,parse_logging_spec};
+    use log::{LogLevel, LogLevelFilter};
+    use super::{FlexiLogger, LogConfig, parse_logging_spec};
 
     fn make_logger(loglevelspec: &'static str) -> FlexiLogger {
         FlexiLogger::new(Some(loglevelspec.to_string()), LogConfig::new()).unwrap()
